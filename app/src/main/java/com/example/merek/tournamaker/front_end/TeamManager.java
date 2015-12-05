fa63e8464096cefb181191331505b0a40ba2ac9c
Conrad <conrad.berlinguette@gmail.com>
2015-12-05 17:16:43 -0500

    UI Drawer

--- app/src/main/java/com/example/merek/tournamaker/front_end/TeamManager.java
+++ app/src/main/java/com/example/merek/tournamaker/front_end/TeamManager.java
@@ -1,21 +1,29 @@
 package com.example.merek.tournamaker.front_end;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.merek.tournamaker.R;
-import com.example.merek.tournamaker.back_end.Tournament;
 
 public class TeamManager extends AppCompatActivity {
 
     //for the drawer
-    private ListView mDrawerList;
-    private ArrayAdapter<String> mAdapter;
+    private ListView drawerList;
+    private ArrayAdapter<String> adapter;
+    private ActionBarDrawerToggle drawerToggle;
+    private DrawerLayout drawerLayout;
+    private String activityTitle;
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +39,21 @@ public class TeamManager extends AppCompatActivity {
         }).start();*/
 
 
-        //setup for drawer
-        mDrawerList = (ListView)findViewById(R.id.navList);
+        //drawer layout title
+        drawerList = (ListView)findViewById(R.id.navList);
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        activityTitle = getTitle().toString();
+
+        //setup for drawer list
         addDrawerItems();
+        setupDrawer();
+
+        //button to get to drawer
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+
+
 
     }
 
@@ -55,11 +75,87 @@ public class TeamManager extends AppCompatActivity {
 
     }
 
-    //the drawer
+
+
+    /*
+    -----The Drawer-----
+     */
+
     private void addDrawerItems() {
-        String[] osArray = { "Back to Home", "Back to Tournament", "Load Tournament"};
-        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
-        mDrawerList.setAdapter(mAdapter);
+        String[] osArray = { "Home", "Team Manager", "Back to Tournament"};
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
+        drawerList.setAdapter(adapter);
+
+        drawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //Toast.makeText(TeamManager.this, "Time for an upgrade!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    private void setupDrawer() {
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
+                R.string.drawer_open, R.string.drawer_close) {
+
+             //Called when a drawer has settled in a completely open state
+             public void onDrawerOpened(View drawerView) {
+                 super.onDrawerOpened(drawerView);
+                 getSupportActionBar().setTitle("Navigate");
+                 invalidateOptionsMenu();
+            }
+
+            //Called when a drawer has settled in a completely closed state.
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(activityTitle);
+                invalidateOptionsMenu();
+            }
+        };
+
+        drawerToggle.setDrawerIndicatorEnabled(true);
+        drawerLayout.setDrawerListener(drawerToggle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        // Activate the navigation drawer toggle
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
